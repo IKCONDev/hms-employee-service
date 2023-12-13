79b73861d931ef49a73fd16ec07e6251bddeb938
Mahesh Jarati <mahesh.jarati@ikcontech.com>
2023-12-13 19:30:16 +0530

    Initial Implementation

---
+++ src/main/java/com/ikn/hms/employee/HmsEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ikn.hms.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsEmployeeServiceApplication.class, args);
+	}
+
+}
